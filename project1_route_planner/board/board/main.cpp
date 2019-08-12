766b95ce2c6c1a21802e98d79f7edd6d052f5162
Diogo Sardinha <diogosar@gmail.com>
2019-08-12 19:38:42 +0100

    Update the program to store a board of State variables. Add CellString function.

--- project1_route_planner/board/board/main.cpp
+++ project1_route_planner/board/board/main.cpp
@@ -1,4 +1,4 @@
-#include <fstream>
+﻿#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,48 +10,76 @@ using std::istringstream;
 using std::string;
 using std::vector;
 
+enum class State
+{
+	kEmpty,
+	kObstacle
+};
+
 // ParseLine function accepts a string as an argument.
 // Use istringstream to parse a line by comma chars, and store the ints in a vector<int>.
 // ParseLine should return the vector after parsing.
-vector<int> ParseLine(string line)
+vector<State> ParseLine(string line)
 {
 	istringstream my_stream(line);
-	vector<int> v;
+	vector<State> v;
 	char c;
 	int n;
-	while (my_stream >> n >> c)
+	while (my_stream >> n >> c && c == ',')
 	{
-		v.push_back(n);
-		//v.push_back(c);
-		// Uncomment to include ',' comas
+		switch (n)
+		{
+		case 0:
+			v.push_back(State::kEmpty);
+			break;
+		default:
+			v.push_back(State::kObstacle);
+			break;
+		}
 	}
 	return v;
 }
 
 // This function should accept a string argument, which represents the path to the file
-vector<vector<int>> ReadBoardFile(string path)
+// use ParseLine and return the board as a vector<vector<int>>
+vector<vector<State>> ReadBoardFile(string path)
 {
-	vector<vector<int>> board;
+	vector<vector<State>> board;
 	ifstream myfile(path);
 	if (myfile)
 	{
 		string line;
 		while (getline(myfile, line))
 		{
-			vector<int> row = ParseLine(line);
+			vector<State> row = ParseLine(line);
 			board.push_back(row);
 		}
 	}
 	return board;
 }
 
-void PrintBoard(const vector<vector<int>> board)
+// CellString function accepts a State as input
+// and returns the following strings:
+// "⛰️   " if the input equals a kObstacle,
+//  "0   " otherwise.
+string CellString(State cell)
+{
+	switch (cell)
+	{
+	case State::kObstacle:
+		return "⛰️   ";
+	default:
+		return "0   ";
+	}
+}
+
+void PrintBoard(const vector<vector<State>> board)
 {
 	for (int i = 0; i < board.size(); i++)
 	{
 		for (int j = 0; j < board[i].size(); j++)
 		{
-			cout << board[i][j];
+			cout << CellString(board[i][j]);
 		}
 		cout << "\n";
 	}
@@ -61,9 +89,9 @@ void PrintBoard(const vector<vector<int>> board)
 
 int main()
 {
-	vector<vector<int>> board;
+	// vector<vector<int>> board;
 	//string s = "files/1.board"; // for linux command line this path is enough
-	string s = "C:/Users/diogo/OneDrive/Work/CS/OOP/cpp-code/project1_route_planner/board/board/files/1.board";
-	board = ReadBoardFile(s);
+	string s = "C:/Users/diogo/OneDrive/Work/CS/OOP/cpp-code/test/test/files/1.board"; 
+	auto board = ReadBoardFile(s);
 	PrintBoard(board);
 }
