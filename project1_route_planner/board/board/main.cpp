983c1a629b984a67a68d795a6d419a6f888a6912
Diogo Sardinha <diogosar@gmail.com>
2019-08-27 20:09:55 +0100

    Add ExpandNeighbors function to expand the A* Search to neighbor nodes and add valid neighbors to the open vector. Add TestExpandNeighbors to test.cpp

--- project1_route_planner/board/board/main.cpp
+++ project1_route_planner/board/board/main.cpp
@@ -21,6 +21,11 @@ enum class State
 	kPath
 };
 
+// Directional deltas in the form of a 2D array.
+// An array is a C++ container much like a vector, although without the ability to change size after initialization. Arrays can be accessed and iterated over just as vectors.
+// Iterate over these delta values to check the neighbors in each direction:
+const int delta[4][2]{ {-1, 0}, {0, -1}, {1, 0}, {0, 1} };
+
 // ParseLine function accepts a string as an argument.
 // Use istringstream to parse a line by comma chars, and store the ints in a vector<int>.
 // ParseLine should return the vector after parsing.
@@ -146,6 +151,32 @@ void AddToOpen(int x, int y, int g, int h, vector<vector<int>>& open, vector<vec
 }
 
 /**
+ * Expand current nodes's neighbors and add them to the open list.
+ * ExpandNeighbors function that accepts references to the following: The current node, the open vector, the grid, and an int array for the goal coordinates.
+ */
+void ExpandNeighbors(const vector<int>& current_node, int goal_point[2], vector<vector<int>>& open, vector<vector<State>>& grid)
+{
+	// Get current node's data.
+	int x = current_node[0];
+	int y = current_node[1];
+	int g = current_node[2];
+	// Loop through current node's potential neighbors.
+	for (int i = 0; i < 4; i++)
+	{
+		int x2 = x + delta[i][0];
+		int y2 = y + delta[i][1];
+		// Check that the potential neighbor's x2 and y2 values are on the grid and not closed.
+		if (CheckValidCell(x2, y2, grid))
+		{
+			// Increment g value, compute h value, and add neighbor to open list.
+			int g2 = g + 1;
+			int h2 = Heuristic(x2, y2, goal_point[0], goal_point[1]);
+			AddToOpen(x2, y2, g2, h2, open, grid);
+		}
+	}
+}
+
+/**
  * Implementation of A* search algorithm.
  *  A* search works by sorting the open list using the f-value, and using the node with the lowest f-value as the next node in the search. This process continues until the goal node has been found or the open list runs out of nodes to use for searching.
  * Search function which takes a board grid and two length 2 int arrays as arguments.
@@ -179,8 +210,8 @@ vector<vector<State>> Search(vector<vector<State>> grid, int initial_point[2], i
 		{
 			return grid;
 		}
-		// If we're not done, expand search to current node's neighbors. This step will be completed later.
-		// ExpandNeighbors
+		// If we're not done, expand search to current node's neighbors.
+		ExpandNeighbors(current, goal_point, open, grid);
 	}
 	// We've run out of new nodes to explore and haven't found a path.
 	vector<vector<State>> board{};
@@ -238,4 +269,5 @@ int main()
 	TestCompare();
 	TestSearch();
 	TestCheckValidCell();
+	TestExpandNeighbors();
 }
--- project1_route_planner/board/board/test.cpp
+++ project1_route_planner/board/board/test.cpp
@@ -329,3 +329,79 @@ void TestCheckValidCell()
 	cout << "----------------------------------------------------------"
 		<< "\n";
 }
+
+void TestExpandNeighbors()
+{
+	cout << "----------------------------------------------------------"
+		<< "\n";
+	cout << "ExpandNeighbors Function Test: ";
+	vector<int> current{ 4, 2, 7, 3 };
+	int goal[2]{ 4, 5 };
+	vector<vector<int>> open{ {4, 2, 7, 3} };
+	vector<vector<int>> solution_open = open;
+	solution_open.push_back(vector<int>{3, 2, 8, 4});
+	solution_open.push_back(vector<int>{4, 3, 8, 2});
+	vector<vector<State>> grid{
+		 {State::kClosed, State::kObstacle, State::kEmpty, State::kEmpty, State::kEmpty, State::kEmpty},
+		 {State::kClosed, State::kObstacle, State::kEmpty, State::kEmpty, State::kEmpty, State::kEmpty},
+		 {State::kClosed, State::kObstacle, State::kEmpty, State::kEmpty, State::kEmpty, State::kEmpty},
+		 {State::kClosed, State::kObstacle, State::kEmpty, State::kEmpty, State::kEmpty, State::kEmpty},
+		 {State::kClosed, State::kClosed, State::kEmpty, State::kEmpty, State::kObstacle, State::kEmpty} };
+	vector<vector<State>> solution_grid = grid;
+	solution_grid[3][2] = State::kClosed;
+	solution_grid[4][3] = State::kClosed;
+	ExpandNeighbors(current, goal, open, grid);
+	CellSort(&open);
+	CellSort(&solution_open);
+	if (open != solution_open)
+	{
+		cout << "failed"
+			<< "\n";
+		cout << "\n";
+		cout << "Your open list is: "
+			<< "\n";
+		PrintVectorOfVectors(open);
+		cout << "Solution open list is: "
+			<< "\n";
+		PrintVectorOfVectors(solution_open);
+		cout << "\n";
+	}
+	else if (grid != solution_grid)
+	{
+		cout << "failed"
+			<< "\n";
+		cout << "\n";
+		cout << "Your grid is: "
+			<< "\n";
+		PrintVectorOfVectors(grid);
+		cout << "\n";
+		cout << "Solution grid is: "
+			<< "\n";
+		PrintVectorOfVectors(solution_grid);
+		cout << "\n";
+	}
+	else
+	{
+		cout << "passed"
+			<< "\n";
+		cout << "\n";
+		cout << "Your open list is: "
+			<< "\n";
+		PrintVectorOfVectors(open);
+		cout << "Solution open list is: "
+			<< "\n";
+		PrintVectorOfVectors(solution_open);
+		cout << "\n";
+		cout << "Your grid is: "
+			<< "\n";
+		PrintVectorOfVectors(grid);
+		cout << "\n";
+		cout << "Solution grid is: "
+			<< "\n";
+		PrintVectorOfVectors(solution_grid);
+		cout << "\n";
+	}
+	cout << "----------------------------------------------------------"
+		<< "\n";
+	return;
+}
